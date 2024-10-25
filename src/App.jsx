a74be3c15d24c7e40a96ce8c85051c0f6fd22cde
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2024-10-24 22:05:18 -0300

    feature: Add confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import confetti from 'canvas-confetti';
 import './App.css'
 
 const TURNS = {
@@ -56,6 +57,7 @@ function App() {
     setTurn(newTurn);
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
+      confetti();
       setWinner(newWinner);
     } else if (checkEndGame(newBoard)) {
       setWinner(false);
